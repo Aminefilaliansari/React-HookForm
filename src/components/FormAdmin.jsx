80dd2fdcf1f48b870a7f5ad058dfb974f527c61f
Amine Filali Ansari <aminefilaliansari@gmail.com>
2023-05-17 12:20:02 +0100

    fix bug 2

--- src/components/FormAdmin.jsx
+++ src/components/FormAdmin.jsx
@@ -150,7 +150,7 @@ export default function FormAdmin() {
 
           <Controller
           control={control}
-          name={Experience.Company00}
+          name={Experience.Company}
           render={({ field }) => (
             <Input {...field} style={{ width: "50%" }} placeholder="Company" />
           )}    
