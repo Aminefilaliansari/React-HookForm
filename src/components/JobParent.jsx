91b6765a7b36abdcde80a662ead11bbae674061e
Amine Filali Ansari <aminefilaliansari@gmail.com>
2023-05-25 16:43:06 +0100

    test crlf

--- src/components/JobParent.jsx
+++ src/components/JobParent.jsx
@@ -4,6 +4,8 @@ export default function JobParent() {
   return (
     <>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, explicabo.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Eum repellat sit quam nulla veniam facere. Labore esse quaerat porro et.</p>
         <JobChild />
     </>
   );
